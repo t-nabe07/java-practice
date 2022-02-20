f0846ee3c93ef61c21bde6c6b46a21ff546dee0c
t-nabe07 <watanabe.t.job@gmail.com>
2022-02-20 11:52:10 +0900

    java練習

---
+++ src/cupsule/human/HumanNoCapsule.java
@@ -0,0 +1,11 @@
+package cupsule.human;
+
+public class HumanNoCapsule {
+	public String name;
+	public int age;
+	
+	public HumanNoCapsule(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+}
