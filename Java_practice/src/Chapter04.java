79681d67b6d8adc73ef98a413d070154b581ce77
t-nabe07 <watanabe.t.job@gmail.com>
2022-03-29 16:47:59 +0900

    java practice

---
+++ Java_practice/src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 7;
+		String str = "nabesan3";
+		System.out.println(str);
+		str = "Never GiveUp!!";
+		System.out.println(str);
+		System.out.println(LUCKY_NUMBER);
+	}
+}
